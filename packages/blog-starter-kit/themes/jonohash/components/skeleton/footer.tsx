794b07d37428651434eedcdb3b173a338eda9a0f
Joncoded <joncoded@gmail.com>
2023-11-16 15:07:00 -0500

    cleaning footer code

--- packages/blog-starter-kit/themes/jonohash/components/skeleton/footer.tsx
+++ packages/blog-starter-kit/themes/jonohash/components/skeleton/footer.tsx
@@ -1,14 +1,11 @@
-import Link from 'next/link'
 import { Container } from './container'
-// import { useAppContext } from './contexts/appContext'
 import { SocialLinks } from '../partials/social-links'
 
 export const Footer = () => {
-	// const { publication } = useAppContext()
 	return (
-		<footer className="bg-white dark:bg-black text-black dark:bg-black w-full border-t mt-20 py-10 md:py-12 dark:border-neutral-800">
+		<footer className="bg-white dark:bg-black text-black dark:bg-black w-full border-t dark:border-neutral-800 mt-20 py-10 md:py-12">
 			<Container className="flex items-center justify-between gap-5 px-5">
-				<SocialLinks />										
+				<SocialLinks />
 			</Container>
 		</footer>
 	)
