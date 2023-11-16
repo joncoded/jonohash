b48fbd67deb07de58f6eb722e2af4fb0f5ea57b7
Joncoded <joncoded@gmail.com>
2023-11-16 15:05:32 -0500

    updating footer background

--- packages/blog-starter-kit/themes/jonohash/components/skeleton/footer.tsx
+++ packages/blog-starter-kit/themes/jonohash/components/skeleton/footer.tsx
@@ -6,7 +6,7 @@ import { SocialLinks } from '../partials/social-links'
 export const Footer = () => {
 	// const { publication } = useAppContext()
 	return (
-		<footer className="bg-white dark:bg-black text-black dark:bg-white w-full border-t mt-20 py-10 md:py-12 dark:border-neutral-800">
+		<footer className="bg-white dark:bg-black text-black dark:bg-black w-full border-t mt-20 py-10 md:py-12 dark:border-neutral-800">
 			<Container className="flex items-center justify-between gap-5 px-5">
 				<SocialLinks />										
 			</Container>
