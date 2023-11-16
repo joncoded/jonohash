6d8c5bdf1c67bc4b21db86b217839b54af180263
Joncoded <joncoded@gmail.com>
2023-11-16 15:05:43 -0500

    updating newsletter form

--- packages/blog-starter-kit/themes/jonohash/pages/index.tsx
+++ packages/blog-starter-kit/themes/jonohash/pages/index.tsx
@@ -29,9 +29,9 @@ import {
 } from '../generated/graphql';
 import { DEFAULT_COVER } from '../utils/const';
 
-// const SubscribeForm = dynamic(() =>
-// 	import('../components/subscribe-form').then((mod) => mod.SubscribeForm),
-// );
+const SubscribeForm = dynamic(() =>
+	import('../components/partials/subscribe-form').then((mod) => mod.SubscribeForm),
+);
 
 const GQL_ENDPOINT = process.env.NEXT_PUBLIC_HASHNODE_GQL_ENDPOINT;
 
@@ -157,16 +157,16 @@ export default function Index({ publication, initialAllPosts, initialPageInfo }:
 						</div>
 
 						{/* newsletter */}
-						{/* {allPosts.length > 0 && (
-							<div className="bg-primary-50 grid grid-cols-4 rounded-lg px-5 py-5 dark:bg-neutral-900 md:py-10">
+						{allPosts.length > 0 && (
+							<div className="bg-primary-200 grid grid-cols-4 rounded-lg px-5 py-5 dark:bg-neutral-900 md:py-10">
 								<div className="col-span-full md:col-span-2 md:col-start-2">
-									<h2 className="text-primary-600 dark:text-primary-500 mb-5 text-center text-lg font-semibold">
-										Subscribe to our newsletter for updates and changelog.
-									</h2>
+									<p className="text-primary-600 dark:text-primary-500 mb-5 text-center text-lg font-semibold">
+										Subscribe to our newsletter for updates!
+									</p>
 									<SubscribeForm />
 								</div>
 							</div>
-						)} */}
+						)}
 
 						{/* more posts */}
 						{morePosts.length > 0 && (
