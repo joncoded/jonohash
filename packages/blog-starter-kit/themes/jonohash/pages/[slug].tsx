ab853ab0b6cf8cf810c21ea35d008e2f0d0e2818
Joncoded <joncoded@gmail.com>
2023-11-17 10:16:45 -0500

    widening post content

--- packages/blog-starter-kit/themes/jonohash/pages/[slug].tsx
+++ packages/blog-starter-kit/themes/jonohash/pages/[slug].tsx
@@ -140,7 +140,7 @@ const Post = (publication: PublicationFragment, post: PostFullFragment) => {
 			{post.features.tableOfContents.isEnabled && <PostTOC />}
 			<MarkdownToHtml contentMarkdown={post.content.markdown} />
 			{(post.tags ?? []).length > 0 && (
-				<div className="mx-auto w-full px-5 text-slate-600 dark:text-neutral-300 md:max-w-screen-md">
+				<div className="mx-auto w-full px-5 text-slate-600 dark:text-neutral-300 lg:max-w-screen-lg">
 					<ul className="flex flex-row flex-wrap items-center gap-2">{tagsList}</ul>
 				</div>
 			)}
